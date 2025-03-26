26c655fcf7a24afe7c6cc47368caf539e5415943
ewenosdu29 <ewen.helary@isen-ouest.yncrea.fr>
2025-03-26 14:02:50 +0100

    ia 5 epochs

--- ia/train_model.py
+++ ia/train_model.py
@@ -4,4 +4,4 @@ dataset_yaml = "D:/ISEN/M1/ProjetM1/DATASET/Chick-Care_V2.v1i.yolov8/data.yaml"
 
 model = YOLO('yolov8n.yaml')
 
-model.train(data=dataset_yaml, epochs=1, imgsz=640)
+model.train(data=dataset_yaml, epochs=5, imgsz=640)
