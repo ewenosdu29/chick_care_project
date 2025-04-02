5a82a6b648831e2f3fbf485ae0d018c51c92c58f
fra-2107 <56804239+fra-2107@users.noreply.github.com>
2025-04-02 09:10:19 +0200

    Increases training epochs and adjusts confidence threshold
    Extends training epochs from 50 to 100 for improved model accuracy.
    Sets confidence threshold to 0.3 to refine object detection results.

--- ia/train_model.py
+++ ia/train_model.py
@@ -4,4 +4,4 @@ dataset_yaml = "C:\\Users\\timch\\Documents\\AAACours\\nini\\Chick-Care_V2.v2i.y
 
 model = YOLO('yolov8n.yaml')
 
-model.train(data=dataset_yaml, epochs=50, imgsz=640)
+model.train(data=dataset_yaml, epochs=100, imgsz=640, conf=0.3)
